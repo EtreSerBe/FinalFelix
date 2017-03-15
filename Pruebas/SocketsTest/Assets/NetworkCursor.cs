eb6fba151ea1d289127e049164a79ba049b29d14
EtreSerBe <augonzalez@uartesdigitales.edu.mx>
2017-03-15 12:04:20 -0600

    Carpeta socket test con ejemplo casi funcionando, listo para probar en dos maquinas

---
+++ Pruebas/SocketsTest/Assets/NetworkCursor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkCursor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        while (true)
+        {
+            MessageData msg = Server.PopMessage();
+            if (msg == null)
+                break;
+
+            transform.position = new Vector3(msg.mousex, msg.mousey, transform.position.z);
+        }
+
+	}
+}
---
+++ Pruebas/SocketsTest/Assets/SendNetworkCursor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SendNetworkCursor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Create the message it and send it off!
+        MessageData msgData = new MessageData();
+        msgData.mousex = Input.mousePosition.x / Screen.width;
+        msgData.mousey = Input.mousePosition.y / Screen.height;
+        msgData.stringData = "Hello World";
+
+        Client.Send(msgData);
+	}
+}
