65f1ccdddeaa35072890aa50851a64c37ea3305d
Luis Eduardo Villela <Luis Eduardo Villela>
2017-03-31 14:26:17 -0600

    Menu

---
+++ MenuProyectoFinalFelix/MenuProyectoFinal/Assets/Menu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void NuevoJuego()
+	{
+		Application.LoadLevel ("NuevoJuego");
+	}
+
+	public void Opciones()
+	{
+		Application.LoadLevel ("Opciones");
+	}
+
+	public void MenuPrincipal()
+	{
+		Application.LoadLevel ("Menu");
+	}
+
+	public void Salir()
+	{
+		Application.Quit ();
+	}
+}
